ef3b4c661c1cfac634906f374c183609d5080540
ksmkk <133812239+ksmkk@users.noreply.github.com>
2025-12-07 15:55:48 -0300

    cambios de mi pinga

---
+++ scripts/pingStore.ts
@@ -0,0 +1,5 @@
+import { store } from "../redux/store";
+import { setCategory } from "../redux/slices/filterSlice";
+
+store.dispatch(setCategory("marketing"));
+console.log(store.getState().filters);
